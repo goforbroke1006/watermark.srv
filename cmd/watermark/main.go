3d02c5e78bee026214208660ae342827b6726ac4
sergey.cherkesov <sergey.cherkasov@devim.team>
2017-10-26 13:33:27 +0300

    Add watermark to jpeg and png

--- cmd/watermark/main.go
+++ cmd/watermark/main.go
@@ -2,46 +2,47 @@ package main
 
 import (
 	"fmt"
-	osUser "os/user"
-
-	"image"
-	"image/png"
-	"image/jpeg"
 	"io/ioutil"
-
-	"github.com/goforbroke1006/watermarksvc/config"
 	"os"
 
-	_ "image/gif"
 	_ "golang.org/x/image/webp"
+	"image"
 	"image/draw"
+	_ "image/gif"
+	"image/jpeg"
+	"image/png"
+
+	"github.com/goforbroke1006/watermarksvc/config"
 )
 
-func openImage(dirPath string, f os.FileInfo) (string, string, *image.Image) {
-	inFilename := dirPath + "/" + f.Name()
-	inFile, err := os.Open(inFilename)
+func openImage(filename string) (*image.Image, string) {
+	inFile, err := os.Open(filename)
 	doPanic(err)
 	defer inFile.Close()
 
 	sourceImage, mimeType, err := image.Decode(inFile)
 	doPanic(err)
 
-	return inFilename, mimeType, &sourceImage
+	return &sourceImage, mimeType
 }
 
-func addWatermark(watermark image.Image, pic *image.Image) image.Image {
+func addWatermark(watermark *image.Image, pic *image.Image) image.Image {
 	r := image.Rectangle{image.Point{}, (*pic).Bounds().Size()}
-	r2 := image.Rectangle{image.Point{}, watermark.Bounds().Size()}
+	r2 := image.Rectangle{image.Point{}, (*watermark).Bounds().Size()}
 
 	rgba := image.NewRGBA(r)
 
 	draw.Draw(rgba, (*pic).Bounds(), *pic, image.Point{0, 0}, draw.Src)
-	draw.Draw(rgba, r2, watermark, image.Point{0, 0}, draw.Over)
+	draw.Draw(rgba, r2, *watermark, image.Point{0, 0}, draw.Over)
 
 	return rgba
 }
 
-func saveFile(outfile *os.File, dist image.Image, mimeType string) {
+func saveFile(filename string, dist image.Image, mimeType string) {
+	outfile, err := os.Create(filename)
+	doPanic(err)
+	defer outfile.Close()
+
 	if "jpeg" == mimeType {
 		jpeg.Encode(outfile, dist, nil)
 	} else if "png" == mimeType {
@@ -50,34 +51,21 @@ func saveFile(outfile *os.File, dist image.Image, mimeType string) {
 }
 
 func main() {
-	user, err := osUser.Current()
-	doPanic(err)
-
-	fmt.Println("Hello, " + user.Name)
-
 	cfg, _ := config.LoadConfig("./config.yml")
 
-	wmFile, err := os.Open(cfg.WatermarkFile)
-	doPanic(err)
-	defer wmFile.Close()
+	wmImage, _ := openImage(cfg.WatermarkFile)
 
-	wmImage, _, err := image.Decode(wmFile)
+	files, err := ioutil.ReadDir(cfg.InputDir)
 	doPanic(err)
 
-	files, err := ioutil.ReadDir(cfg.InputDir);
-	doPanic(err)
 	for _, f := range files {
-		inFilename, mimeType, sourceImage := openImage(cfg.InputDir, f)
+		inFilename := cfg.InputDir + "/" + f.Name()
+		sourceImage, mimeType := openImage(inFilename)
 		fmt.Println(inFilename + " (" + mimeType + ")")
 
 		dist := addWatermark(wmImage, sourceImage)
 
-		outfile, err := os.Create(cfg.OutputDir + "/" + f.Name())
-		doPanic(err)
-
-		saveFile(outfile, dist, mimeType)
-
-		outfile.Close()
+		saveFile(cfg.OutputDir+"/"+f.Name(), dist, mimeType)
 	}
 }
 
